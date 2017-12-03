20262a0f283dee34004811b00e7fba55b3817b69
Ajeet D'Souza <98ajeet@gmail.com>
2017-12-03 11:19:53 +0530

    Changed graph class to somewhat match the original approach.

--- lib/networkx/graph.rb
+++ lib/networkx/graph.rb
@@ -1,40 +1,29 @@
 module NetworkX
   class Graph
     def initialize
-      @nodes = Set.new
-      @edges = {}
-    end
-
-    def add_edge(node_1, node_2, **attrs)
-      add_node(node_1)
-      add_node(node_2)
-
-      edge_key         = Set.new([node_1, node_2])
-      @edges[edge_key] = attrs
-    end
-
-    def add_edges(edges)
-      case edges
-      when Array
-        edges.each { |node_1, node_2, **attrs| add_edge(node_1, node_2, **attrs) }
-      else
-        raise ArgumentError, 'Expected argument to be an Array of edges, '\
-                             "received #{edges.class.name} instead."
-      end
+      @adj = Hash.new
     end
 
     def add_node(node)
-      @nodes.add(node)
-    end
-
-    def add_nodes(nodes)
-      case nodes
-      when Set, Array
-        nodes.each { |node| add_node(node) }
-      else
-        raise ArgumentError, 'Expected argument to be an Array or Set of nodes, '\
-                             "received #{nodes.class.name} instead."
+      unless @adj.has_key? node
+        @adj[node] = Hash.new
       end
     end
+
+    def add_nodes_from(nodes)
+      nodes.each {|node| add_node(node)}
+    end
+
+    def add_edge(node1, node2, **attrs)
+      add_node(node1)
+      add_node(node2)
+
+      @adj[node1][node2] = attrs
+    end
+
+    def add_edges_from(edges)
+      edges.each {|edge| add_edge(*edge)}
+    end
+
   end
 end
