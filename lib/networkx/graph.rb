195cddec11d6a148bcd62f882e2b6bbd8aa6c9b2
Ajeet D'Souza <98ajeet@gmail.com>
2017-12-03 14:04:01 +0530

    Fixed graph to follow style guide.

--- lib/networkx/graph.rb
+++ lib/networkx/graph.rb
@@ -1,51 +1,44 @@
 module NetworkX
   class Graph
     def initialize
-      @adj = Hash.new
+      @adj = {}
     end
 
     def add_node(node)
-      unless @adj.has_key? node
-        @adj[node] = Hash.new
-      end
+      @adj[node] = {} unless @adj.key? node
     end
 
     def add_nodes_from(nodes)
-      nodes.each {|node| add_node(node)}
+      nodes.each { |node| add_node(node) }
     end
 
-    def add_edge(node1, node2, **attrs)
-      add_node(node1)
-      add_node(node2)
+    def add_edge(node_1, node_2, **attrs)
+      add_node(node_1)
+      add_node(node_2)
 
-      @adj[node1][node2] = attrs
+      @adj[node_1][node_2] = attrs
     end
 
     def add_edges_from(edges)
-      edges.each {|edge| add_edge(*edge)}
+      edges.each { |edge| add_edge(*edge) }
     end
 
     def remove_node(node)
       @adj.delete(node)
-      @adj.each_key {|key| @adj[key].delete(node)}
+      @adj.each_key { |key| @adj[key].delete(node) }
     end
 
     def remove_nodes_from(nodes)
-      nodes.each do |node|
-        @adj.delete(node)
-      end
-        nodes.each do |node|
-        @adj.each_key {|key| @adj[key].delete(node)}
-      end
+      nodes.each { |node| @adj.delete(node) }
+      nodes.each { |node| @adj.each_key { |key| @adj[key].delete(node) } }
     end
 
-    def remove_edge(node1, node2)
-      @adj[node1].delete(node2) if @adj.has_key?node1
+    def remove_edge(node_1, node_2)
+      @adj[node_1].delete(node_2) if @adj.key? node_1
     end
 
     def remove_edges_from(edges)
-      edges.each {|edge| remove_edge(*edge)}
+      edges.each { |edge| remove_edge(*edge) }
     end
-
   end
 end
