b64f662745fdc1d9721b13e66aa1db3b8f8137fd
npochhi <pochhi.nilay@gmail.com>
2018-05-07 23:46:28 +0530

    Completed graph.rb

--- lib/networkx/graph.rb
+++ lib/networkx/graph.rb
@@ -117,5 +117,141 @@ module NetworkX
                              "received #{nodes.class.name} instead."
       end
     end
+
+    def remove_edge(node_1, node_2)
+      raise KeyError, "#{node_1} is not a valid node." unless @nodes.key?(u)
+      raise KeyError, "#{node_2} is not a valid node" unless @nodes.key?(v)
+      raise KeyError, "The given edge is not a valid one." unless @adj[node_1].key?(node_2)
+      @adj[node_1].delete(node_2)
+      if node_1 != node_2
+        @[node_2].delete(node_1)
+      end
+    end
+
+    def remove_edges(edges)
+      case edges
+      when Array, Set
+        edges.each { |node_1, node_2| remove_edge(node_1, node_2) }
+      else
+        raise ArgumentError, "Expected Arguement to be Array or Set of edges, "\
+                             "received #{edges.class.name} instead."
+      end
+    end
+
+    def add_weighted_edge(node_1, node_2, weight)
+      add_edge(node_1, node_2, weight: weight)
+    end
+
+    def add_weighted_edges(edges, weights)
+      raise ArgumentError, "edges and weights array must have equal number of elements." unless edges.size == weights.size
+      if edges.is_a?(Array) && weights.is_a?(Array)
+        (edges.transpose << weights).transpose.each { |node_1, node_2, weight|
+          add_weighted_edge(node_1, node_2, weight)
+        }
+      else
+        raise ArgumentError, "edges and weight must be given in an Array."
+      end
+    end
+
+    def clear
+      @adj.clear
+      @nodes.clear
+      @graph.clear
+    end
+
+    def nodes
+      @nodes
+    end
+
+    def adj
+      @adj
+    end
+
+    def has_node(node)
+      @node.key?(node)
+    end
+
+    def has_edge(node_1, node_2)
+      return true if @nodes.key?(node_1) and @adj[node_1].key?(node_2)
+      return false
+    end
+
+    def get_node_data(node)
+      return @nodes[node] if @nodes.key?(node)
+      raise ArgumentError, "No such node exists!"
+    end
+
+    def get_edge_data(node_1, node_2)
+      return @adj[node_1][node_2] if @nodes.key?(node_1) and @adj[node_1].key?(node_2)
+      raise ArgumentError, "No such edge exists!"
+    end
+
+    def neighbours(node)
+      return @adj[node] if @nodes.key?(node)
+      raise ArgumentError, "No such node exists!"
+    end
+
+    def number_of_nodes
+      return @nodes.length
+    end
+
+    def number_of_edges
+      num = 0
+      @adj.each { |u| num += u.length }
+      return num
+    end
+
+    def size(is_weighted=false)
+      if is_weighted
+        graph_size = 0
+        @adj.each { |u|
+          u.each { |v|
+            graph_size += v[:weight] if v.key?(weight)
+          }
+        }
+        return graph_size
+      else
+        return number_of_edges
+      end
+    end
+
+    def subgraph(nodes)
+      case nodes
+      when Array, Set
+        subGraph = NetworkX::Graph.new(@graph)
+        nodes.each { |u| 
+          if @nodes.key?(u)
+            subGraph.add_node(u, @nodes[u])
+            @adj[u].each { |v|
+              subGraph.add_edge(u, v, @adj[u][v]) if @adj[u].key?(v)
+            }
+            return subGraph
+          else
+            raise ArgumentError, "#{u} does not exist in the current graph!"
+          end
+        }
+      else
+        raise ArgumentError, "Expected Argument to be Array or Set of nodes, "\
+                             "received #{nodes.class.name} instead."
+      end
+    end
+
+    def edge_subgraph(edges)
+      case edges
+      when Array, Set
+        subGraph = NetworkX::Graph.new(@graph)
+        edges.each { |u, v|
+          raise ArgumentError, "#{u} does not exist in the graph!" unless @nodes.key?(u)
+          raise ArgumentError, "#{v} does not exist in the graph!" unless @nodes.key?(v)
+          raise ArgumentError, "Edge between #{u} and #{v} does not exist in the graph!" unless @adj[u].key?(v)
+          subGraph.add_node(u, @nodes[u])
+          subGraph.add_node(v, @nodes[v])
+          subGraph.add_edge(u, v, @adj[u][v])
+        }
+        return subGraph
+      else
+        raise ArgumentError, "Expected Argument to be Array or Set of edges, "\
+        "received #{edges.class.name} instead."
+    end
   end
 end
