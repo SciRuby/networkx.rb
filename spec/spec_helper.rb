3337944e840fd217e8d62c4cf0f8b62c74000b24
universato <universato@gmail.com>
2022-11-12 17:47:01 +0900

    Refactoring for spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
 require 'simplecov'
-SimpleCov.start do
-  add_filter 'spec'
-  # minimum_coverage_by_file 95
-end
+SimpleCov.start{ add_filter 'spec' }
 
 require 'bundler/setup'
 
@@ -16,27 +13,5 @@ RSpec::Expectations.configuration.warn_about_potential_false_positives = false
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
-end
-
-class String
-  # allows to pretty test agains multiline strings:
-  #   %Q{
-  #     |test
-  #     |me
-  #   }.unindent # =>
-  # "test
-  # me"
-  def unindent
-    gsub(/\n\s+?\|/, "\n") # for all lines looking like "<spaces>|" -- remove this.
-      .gsub(/\|\n/, "\n")  # allow to write trailing space not removed by editor
-      .gsub(/^\n|\n\s+$/, '') # remove empty strings before and after
-  end
+  config.example_status_persistence_file_path = 'spec/.rspec_status'
 end
