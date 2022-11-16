5c83f6eb32a47c346961c9f21c44b198a1b1e2a2
universato <universato@gmail.com>
2022-11-17 08:24:05 +0900

    Remove duplicated test code

--- lib/networkx/flow/utils.rb
+++ lib/networkx/flow/utils.rb
@@ -67,24 +67,6 @@ module NetworkX
     edge_list = []
 
     graph.adj.each do |u, u_edges|
-      require_relative '../../../spec/spec_helper' # require 'spec_helper'
-      RSpec.describe NetworkX::DiGraph do
-        subject { graph }
-
-        let(:graph) { described_class.new }
-
-        before do
-          graph.add_edge(1, 2)
-          graph.add_edge(2, 4)
-        end
-
-        context 'when capacity_scaling is called' do
-          subject { NetworkX.capacity_scaling(graph) }
-
-          it { is_expected.to eq([0, {1 => {2 => 0}, 2 => {4 => 0}, 4 => {}}]) }
-        end
-      end
-
       u_edges.each do |v, uv_attrs|
         edge_list << [u, v, uv_attrs] if (uv_attrs[:capacity] || inf).positive? && u != v
       end
