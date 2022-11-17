845248224fa27bb5b937958e79944648833bf53c
universato <universato@gmail.com>
2022-11-17 14:51:16 +0900

    Add test to nagative_edge_cycle() for coverage

--- spec/network_x/flow/digraph_capacityscaling_spec.rb
+++ spec/network_x/flow/digraph_capacityscaling_spec.rb
@@ -13,4 +13,11 @@ RSpec.describe NetworkX::DiGraph do
 
     it { is_expected.to eq([0, {1 => {2 => 0}, 2 => {4 => 0}, 4 => {}}]) }
   end
+
+  it 'negative_edge_cycle when Argument Error in this method' do
+    graph = NetworkX::Graph.new
+    graph.add_weighted_edges([[:a, :b], [:b, :a]], [9, -10])
+
+    expect(NetworkX.negative_edge_cycle(graph)).to be_truthy
+  end
 end
