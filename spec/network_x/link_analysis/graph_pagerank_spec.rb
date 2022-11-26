80b3ca2d2c2ae81fcdb2410e813cbcaedb65da81
universato <universato@gmail.com>
2022-11-26 16:06:38 +0900

    Refactoring for pagerank

--- spec/network_x/link_analysis/graph_pagerank_spec.rb
+++ spec/network_x/link_analysis/graph_pagerank_spec.rb
@@ -26,20 +26,20 @@ RSpec.describe NetworkX::Graph do
     graph = NetworkX::DiGraph.new
     graph.add_edges([[1, 2], [1, 3], [2, 4], [3, 4], [3, 5], [4, 5], [5, 1]])
     pagerank = NetworkX.pagerank2(graph, alpha: 0.95)
-    expect(pagerank[1]).to be_within(0.00001).of(0.26261033194706523)
-    expect(pagerank[2]).to be_within(0.00001).of(0.13473925873325307)
-    expect(pagerank[3]).to be_within(0.00001).of(0.13473925873325307)
-    expect(pagerank[4]).to be_within(0.00001).of(0.20200447126193546)
-    expect(pagerank[5]).to be_within(0.00001).of(0.26590667932449313)
+    expect(pagerank[1]).to be_within(1e-6).of(0.26261033194706523)
+    expect(pagerank[2]).to be_within(1e-6).of(0.13473925873325307)
+    expect(pagerank[3]).to be_within(1e-6).of(0.13473925873325307)
+    expect(pagerank[4]).to be_within(1e-6).of(0.20200447126193546)
+    expect(pagerank[5]).to be_within(1e-6).of(0.26590667932449313)
   end
 
   it 'pagerank2' do
     graph = NetworkX::Graph.new
     graph.add_edges([[1, 2], [2, 4]])
     pagerank = NetworkX.pagerank2(graph)
-    expect(pagerank[1]).to be_within(0.0001).of(0.2567467074324632)
-    expect(pagerank[2]).to be_within(0.0001).of(0.4865065851350735)
-    expect(pagerank[4]).to be_within(0.0001).of(0.2567467074324632)
+    expect(pagerank[1]).to be_within(1e-4).of(0.2567467074324632)
+    expect(pagerank[2]).to be_within(1e-4).of(0.4865065851350735)
+    expect(pagerank[4]).to be_within(1e-4).of(0.2567467074324632)
   end
 
   it 'pagerank2(alpha: 0)' do
