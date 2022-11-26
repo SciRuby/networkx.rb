64918a909d50e8fbc3fb5024b054147e6aafd663
universato <universato@gmail.com>
2022-11-26 16:28:44 +0900

    Update pagerank

--- lib/networkx/link_analysis/pagerank.rb
+++ lib/networkx/link_analysis/pagerank.rb
@@ -9,7 +9,7 @@ module NetworkX
   # @param max_iter [Integer] max iterations for the pagerank algorithm to run
   #
   # @return [Array<Numeric>] pagerank values of the graph
-  def self.pagerank1(graph, init: nil, alpha: 0.85, eps: 1e-4, max_iter: 100)
+  def self.pagerank1(graph, init: nil, alpha: 0.85, eps: 1e-6, max_iter: 100)
     dim = graph.nodes.length
     if init.nil?
       init = graph.nodes(data: false).to_h{ |i| [i, 1.0 / dim] }
--- spec/network_x/link_analysis/graph_pagerank_spec.rb
+++ spec/network_x/link_analysis/graph_pagerank_spec.rb
@@ -1,45 +1,40 @@
 RSpec.describe NetworkX::Graph do
-  subject { graph }
+  it 'pagerank1' do
+    graph = NetworkX::Graph.new
+    graph.add_edges([[1, 2], [2, 4]])
 
-  let(:graph) { described_class.new }
-
-  before do
-    graph.add_edge(1, 2)
-    graph.add_edge(2, 4)
+    pagerank = NetworkX.pagerank1(graph)
+    expect(pagerank[0]).to be_within(1e-6).of(0.25675675672536513)
+    expect(pagerank[1]).to be_within(1e-6).of(0.4864864865492695)
+    expect(pagerank[2]).to be_within(1e-6).of(0.25675675672536513)
   end
 
-  context 'when pagerank is called' do
-    subject { NetworkX.pagerank(graph, init: {1 => 0.333, 2 => 0.333, 4 => 0.333}) }
-
-    it { is_expected.to eq([0.2567467074324632, 0.4865065851350735, 0.2567467074324632]) }
-  end
-
-  it 'error of pagerank' do
+  it 'error of pagerank1' do
     graph = NetworkX::Graph.new
     graph.add_edges([[1, 2], [1, 3], [2, 4], [3, 4], [3, 5], [4, 5], [5, 1]])
     init = (1..5).to_h{|i| [i, 0.20] }
-    expect{NetworkX.pagerank(graph, init: init, max_iter: 5)}.to raise_error(ArgumentError)
-    expect{NetworkX.pagerank(graph, eps: 1e-16)}.to raise_error(ArgumentError)
+    expect{NetworkX.pagerank1(graph, init: init, max_iter: 5)}.to raise_error(ArgumentError)
+    expect{NetworkX.pagerank1(graph, eps: 1e-16)}.to raise_error(ArgumentError)
   end
 
   it 'Digraph#pagerank2' do
     graph = NetworkX::DiGraph.new
     graph.add_edges([[1, 2], [1, 3], [2, 4], [3, 4], [3, 5], [4, 5], [5, 1]])
     pagerank = NetworkX.pagerank2(graph, alpha: 0.95)
-    expect(pagerank[1]).to be_within(1e-6).of(0.26261033194706523)
-    expect(pagerank[2]).to be_within(1e-6).of(0.13473925873325307)
-    expect(pagerank[3]).to be_within(1e-6).of(0.13473925873325307)
-    expect(pagerank[4]).to be_within(1e-6).of(0.20200447126193546)
-    expect(pagerank[5]).to be_within(1e-6).of(0.26590667932449313)
+    expect(pagerank[1]).to be_within(1e-6).of(0.26261029498162486)
+    expect(pagerank[2]).to be_within(1e-6).of(0.13473989208216358)
+    expect(pagerank[3]).to be_within(1e-6).of(0.13473989208216358)
+    expect(pagerank[4]).to be_within(1e-6).of(0.20200434638512804)
+    expect(pagerank[5]).to be_within(1e-6).of(0.2659055744689199)
   end
 
   it 'pagerank2' do
     graph = NetworkX::Graph.new
     graph.add_edges([[1, 2], [2, 4]])
     pagerank = NetworkX.pagerank2(graph)
-    expect(pagerank[1]).to be_within(1e-4).of(0.2567467074324632)
-    expect(pagerank[2]).to be_within(1e-4).of(0.4865065851350735)
-    expect(pagerank[4]).to be_within(1e-4).of(0.2567467074324632)
+    expect(pagerank[1]).to be_within(1e-6).of(0.25675675672536513)
+    expect(pagerank[2]).to be_within(1e-6).of(0.4864864865492695)
+    expect(pagerank[4]).to be_within(1e-6).of(0.25675675672536513)
   end
 
   it 'pagerank2(alpha: 0)' do
