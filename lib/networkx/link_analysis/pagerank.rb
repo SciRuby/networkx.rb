8927d48a95115a50abfe42d6c78b57a4dc0ef96f
universato <universato@gmail.com>
2022-11-26 16:41:18 +0900

    Remove old pagerank method

--- lib/networkx/link_analysis/pagerank.rb
+++ lib/networkx/link_analysis/pagerank.rb
@@ -1,58 +1,5 @@
 module NetworkX
   # Computes pagerank values for the graph
-  # This is deprecated because it may be slow
-  #
-  # @param graph [Graph] a graph
-  # @param init [Array<Numeric>] initial pagerank values for the nodes
-  # @param alpha [Numeric] the alpha value to compute the pagerank
-  # @param eps [Numeric] tolerence to check for convergence
-  # @param max_iter [Integer] max iterations for the pagerank algorithm to run
-  #
-  # @return [Array<Numeric>] pagerank values of the graph
-  def self.pagerank1(graph, init: nil, alpha: 0.85, eps: 1e-6, max_iter: 100)
-    dim = graph.nodes.length
-    if init.nil?
-      init = graph.nodes(data: false).to_h{ |i| [i, 1.0 / dim] }
-    else
-      s = init.values.sum.to_f
-      init = init.transform_values { |v| v / s }
-    end
-    raise ArgumentError, 'Init array needs to have same length as number of graph nodes!' \
-                          unless dim == init.length
-
-    matrix = []
-    elem_ind = {}
-    p = []
-    curr = init.values
-    init.keys.each_with_index { |n, i| elem_ind[n] = i }
-    graph.adj.each do |_u, u_edges|
-      adj_arr = Array.new(dim, 0)
-      u_edges.each do |v, _|
-        adj_arr[elem_ind[v]] = 1
-      end
-      matrix << adj_arr
-    end
-    (0..(dim - 1)).each do |i|
-      p[i] = []
-      (0..(dim - 1)).each { |j| p[i][j] = matrix[i][j] / (matrix[i].sum * 1.0) }
-    end
-
-    max_iter.times do |_|
-      prev = curr.clone
-      dim.times do |i|
-        ip = 0
-        dim.times { |j| ip += p.transpose[i][j] * prev[j] }
-        curr[i] = (alpha * ip) + ((1 - alpha) / (dim * 1.0))
-      end
-      err = 0
-      dim.times { |i| err += (prev[i] - curr[i]).abs }
-      return curr if err < eps
-    end
-    raise ArgumentError, 'PageRank failed to converge!'
-  end
-
-  # Computes pagerank values for the graph
-  # New pagerank method.
   #
   # @param graph [Graph] a graph
   # @param alpha [Numeric] the alpha value to compute the pagerank
@@ -60,7 +7,7 @@ module NetworkX
   # @param max_iter [Integer] max iterations for the pagerank algorithm to run
   #
   # @return [Hash of Object => Float] pagerank values of the graph
-  def self.pagerank2(graph, alpha: 0.85, personalization: nil, eps: 1e-6, max_iter: 100)
+  def self.pagerank(graph, alpha: 0.85, personalization: nil, eps: 1e-6, max_iter: 100)
     n = graph.number_of_nodes
 
     matrix, index_to_node = NetworkX.to_matrix(graph, 0)
@@ -92,10 +39,7 @@ module NetworkX
       err = (0...n).map{|i| (prev[i] - curr[i]).abs }.sum
       return (0...n).map{|i| [index_to_node[i], curr[i]] }.sort.to_h if err < eps
     end
+    warn "pagerank() failed within #{max_iter} iterations. Please inclease max_iter: or loosen eps:"
     (0...n).map{|i| [index_to_node[i], curr[i]] }.sort.to_h
-  end
-
-  class << self
-    alias pagerank pagerank1
   end
 end
--- spec/network_x/link_analysis/graph_pagerank_spec.rb
+++ spec/network_x/link_analysis/graph_pagerank_spec.rb
@@ -1,26 +1,8 @@
 RSpec.describe NetworkX::Graph do
-  it 'pagerank1' do
-    graph = NetworkX::Graph.new
-    graph.add_edges([[1, 2], [2, 4]])
-
-    pagerank = NetworkX.pagerank1(graph)
-    expect(pagerank[0]).to be_within(1e-6).of(0.25675675672536513)
-    expect(pagerank[1]).to be_within(1e-6).of(0.4864864865492695)
-    expect(pagerank[2]).to be_within(1e-6).of(0.25675675672536513)
-  end
-
-  it 'error of pagerank1' do
-    graph = NetworkX::Graph.new
-    graph.add_edges([[1, 2], [1, 3], [2, 4], [3, 4], [3, 5], [4, 5], [5, 1]])
-    init = (1..5).to_h{|i| [i, 0.20] }
-    expect{NetworkX.pagerank1(graph, init: init, max_iter: 5)}.to raise_error(ArgumentError)
-    expect{NetworkX.pagerank1(graph, eps: 1e-16)}.to raise_error(ArgumentError)
-  end
-
-  it 'Digraph#pagerank2' do
+  it 'Digraph#pagerank' do
     graph = NetworkX::DiGraph.new
     graph.add_edges([[1, 2], [1, 3], [2, 4], [3, 4], [3, 5], [4, 5], [5, 1]])
-    pagerank = NetworkX.pagerank2(graph, alpha: 0.95)
+    pagerank = NetworkX.pagerank(graph, alpha: 0.95, max_iter: 1000)
     expect(pagerank[1]).to be_within(1e-6).of(0.26261029498162486)
     expect(pagerank[2]).to be_within(1e-6).of(0.13473989208216358)
     expect(pagerank[3]).to be_within(1e-6).of(0.13473989208216358)
@@ -28,19 +10,19 @@ RSpec.describe NetworkX::Graph do
     expect(pagerank[5]).to be_within(1e-6).of(0.2659055744689199)
   end
 
-  it 'pagerank2' do
+  it 'pagerank' do
     graph = NetworkX::Graph.new
     graph.add_edges([[1, 2], [2, 4]])
-    pagerank = NetworkX.pagerank2(graph)
+    pagerank = NetworkX.pagerank(graph)
     expect(pagerank[1]).to be_within(1e-6).of(0.25675675672536513)
     expect(pagerank[2]).to be_within(1e-6).of(0.4864864865492695)
     expect(pagerank[4]).to be_within(1e-6).of(0.25675675672536513)
   end
 
-  it 'pagerank2(alpha: 0)' do
+  it 'pagerank(alpha: 0)' do
     graph = NetworkX::Graph.new
     graph.add_edges([[:a, :b], [:b, :c], [:c, :d], [:d, :a]])
-    pagerank = NetworkX.pagerank2(graph, alpha: 0)
+    pagerank = NetworkX.pagerank(graph, alpha: 0)
     expect(pagerank).to eq({a: 0.25, b: 0.25, c: 0.25, d: 0.25})
   end
 end
